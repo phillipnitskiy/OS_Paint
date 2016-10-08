52c235deaf9c3189a78f9573259c788b0d3e308e
Philip <phillipnitskiy@gmail.com>
2016-10-08 15:26:39 +0300

    Corrected

---
+++ OS_Paint/Pen.cpp
@@ -0,0 +1,34 @@
+#include "stdafx.h"
+#include "Pen.h"
+
+Pen::Pen()
+{
+
+}
+
+void Pen::EditShape(POINT x)
+{
+	points.push_back(x);
+}
+
+void Pen::Paint(HDC hdc)
+{
+	int points_size = points.size();
+
+	for (int i = 0; i < points_size; i++) {
+		if (i > 1) {
+			MoveToEx(hdc, points[i - 1].x, points[i - 1].y, NULL);
+			LineTo(hdc, points[i].x, points[i].y);
+		}
+		else
+		{
+			SetPixelV(hdc, points[i].x, points[i].y, RGB(0,0,0));
+		}
+
+	}
+	
+}
+
+Pen::~Pen()
+{
+}
---
+++ OS_Paint/Shape.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "Shape.h"
+
+Shape::Shape()
+{
+}
+
+void Shape::Paint(HDC hdc)
+{
+
+}
+
+void Shape::EditShape(POINT point)
+{
+
+}
+
+Shape::~Shape()
+{
+}
